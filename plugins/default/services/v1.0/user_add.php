f8885d80bf4adeac4db293eb1925e6432f5bfa96
Arsalan Shah <manactric@gmail.com>
2020-05-07 20:12:27 +0500

    Update user_add.php

--- plugins/default/services/v1.0/user_add.php
+++ plugins/default/services/v1.0/user_add.php
@@ -4,7 +4,7 @@
  *
  * @package   Open Source Social Network
  * @author    Open Social Website Core Team <info@softlab24.com>
- * @copyright © SOFTLAB24 LIMITED
+ * @copyright Â© SOFTLAB24 LIMITED
  * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
  * @link      https://www.opensource-socialnetwork.org/
  */
@@ -17,10 +17,6 @@ $user['password']  = input('password');
 $user['gender']    = input('gender');
 $user['birthdate'] = input('birthdate');
 
-/**$fields = ossn_user_fields_names();
-foreach($fields['required'] as $field) {
-		$user[$field] = input($field);
-}**/
 
 if(!empty($user)) {
 		foreach($user as $field => $value) {
@@ -56,12 +52,9 @@ $add->last_name       = $user['lastname'];
 $add->email           = $user['email'];
 $add->password        = $user['password'];
 $add->sendactiviation = true;
+$add->gender          = $user['gender'];
+$add->birthdate       = $user['birthdate'];
 
-foreach($fields as $items) {
-		foreach($items as $field) {
-				$add->{$field} = $user[$field];
-		}
-}
 if(!$add->isUsername()) {
 		$params['OssnServices']->throwError('103', 'username:error');
 }
